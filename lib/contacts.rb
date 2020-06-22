413cc95d93caaca71419b11535485129637dc956
Tomas Perez <tomasitopg6@gmail.com>
2020-06-22 23:11:28 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -22,6 +22,8 @@ def remove_strawberry(contacts)
       
       contact_details_hash.each do | attribute, data |
         
+        if attribute == :favorite_ice_cream_flavors
+        
       end
   
   end
