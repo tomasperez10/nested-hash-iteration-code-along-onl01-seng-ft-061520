f4e04ace41bab951274d7272d09863e966740fb4
Tomas Perez <tomasitopg6@gmail.com>
2020-06-22 23:14:46 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -24,7 +24,7 @@ def remove_strawberry(contacts)
         
         if attribute == :favorite_ice_cream_flavors
           
-          data.delete_if {|ice_cream| ice_cream == "strawberry"
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
         
           end
         end
